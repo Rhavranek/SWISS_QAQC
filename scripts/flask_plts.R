d6f17c07d22b9007dec4d6661e98b45c27cd1fa4
Rachel Havranek <rachelhavranek@Rachels-Mac-mini.local>
2022-02-10 09:44:42 -0700

    102821

--- scripts/flask_plts.R
+++ scripts/flask_plts.R
@@ -3,8 +3,10 @@ Flask_h2o_plt <- function (df){
     ggplot()+
     aes(x = Flask, y = H2O_out, size = 2)+
     geom_point() +
-    scale_y_continuous(limits =c(0, 1500), expand = c(0,0)) +
+    #scale_y_continuous(limits =c(0, 1500), expand = c(0,0)) +
     scale_x_continuous(breaks = c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)) +
     theme_figure()
+  
+  print(plt)
 }
 
