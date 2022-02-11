68a5a868f34969ffd352b113de044bfcff4f9817
Rachel Havranek <rachelhavranek@Rachels-Mac-mini.local>
2022-02-11 11:37:26 -0700

    finished fondue 2021 QAQC

--- scripts/flask_plts.R
+++ scripts/flask_plts.R
@@ -5,7 +5,10 @@ Flask_h2o_plt <- function (df){
     geom_point() +
     #scale_y_continuous(limits =c(0, 1500), expand = c(0,0)) +
     scale_x_continuous(breaks = c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)) +
-    theme_figure()
+    theme_figure()+
+    theme(
+      legend.position = "none"
+    )
   
   print(plt)
 }
