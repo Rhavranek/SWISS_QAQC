3cc1b1c5fc1727ca889ba0ca47edde0fb2d0fe83
Rachel Havranek <rachelhavranek@rgnt2-86-155-dhcp.int.colorado.edu>
2022-02-20 11:22:09 -0700

    started on Meringue, added a faceted isotopes plot

--- scripts/Rachels_custom_plots.R
+++ scripts/Rachels_custom_plots.R
@@ -3,7 +3,6 @@ Flask_h2o_plt <- function (df){
     ggplot()+
     aes(x = Flask, y = H2O_out, size = 2)+
     geom_point() +
-    #scale_y_continuous(limits =c(0, 1500), expand = c(0,0)) +
     scale_x_continuous(breaks = c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)) +
     theme_figure()+
     theme(
@@ -13,18 +12,38 @@ Flask_h2o_plt <- function (df){
   print(plt)
 }
 
+
+Flask_isotopes_plt <- function (df){
+  plt <-df %>% 
+    pivot_longer(cols = c(d180_out, d2H_out)) %>% 
+      ggplot()+
+        aes(x = Flask, y = value, size = 2) +
+        facet_wrap (~name,scales = "free") +
+        geom_point()+
+        scale_x_continuous(breaks = c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)) +
+        theme_figure()+
+        theme(
+          legend.position = "none"
+        )
+  
+  print(plt)
+}
+
+
+
 Flask_d18O_plt <- function (df) {
   plt <- df %>% 
     ggplot()+
     aes(x = Flask, y = d18O_out, size = 2)+
     geom_point() +
-    geom_errorbar(aes(ymin = d18O_out - SD_18O_out, ymax = d18O_out - SD_18O_out))
-    #scale_y_continuous(limits =c(0, 1500), expand = c(0,0)) +
+    geom_errorbar(aes(ymin = d18O_out - SD_18O_out, ymax = d18O_out - SD_18O_out))+
     scale_x_continuous(breaks = c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)) +
     theme_figure()
   
   print(plt)
 }
+
+
 
 OutletValve_plt <- function(df){
   plt <- df %>% 
