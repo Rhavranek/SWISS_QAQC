9a708db55137c90bea34e1df892b857bd8da6df8
Rachel Havranek <rachelhavranek@rgnt2-86-155-dhcp.int.colorado.edu>
2022-02-20 12:23:57 -0700

    fixed d18O and made the d2H vs. d18O plt language

--- scripts/Rachels_custom_plots.R
+++ scripts/Rachels_custom_plots.R
@@ -15,7 +15,7 @@ Flask_h2o_plt <- function (df){
 
 Flask_isotopes_plt <- function (df){
   plt <-df %>% 
-    pivot_longer(cols = c(d180_out, d2H_out)) %>% 
+    pivot_longer(cols = c(d18O_out, d2H_out)) %>% 
       ggplot()+
         aes(x = Flask, y = value, size = 2) +
         facet_wrap (~name,scales = "free") +
@@ -36,13 +36,28 @@ Flask_d18O_plt <- function (df) {
     ggplot()+
     aes(x = Flask, y = d18O_out, size = 2)+
     geom_point() +
-    geom_errorbar(aes(ymin = d18O_out - SD_18O_out, ymax = d18O_out - SD_18O_out))+
+    geom_errorbar(aes(ymin = d18O_out + SD_18O_out, ymax = d18O_out - SD_18O_out))+
     scale_x_continuous(breaks = c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)) +
     theme_figure()
   
   print(plt)
 }
 
+
+dual_isotope_plt <- function (df){
+  plt <- df %>% 
+    ggplot()+
+    aes(x = d18O_out, y = d2H_out)+
+    geom_point(aes(size =2, text = Flask )) +
+    geom_errorbar(aes(ymin = d2H_out + SD_2H_out, ymax = d2H_out - SD_2H_out))+
+    geom_errorbarh(aes(xmin = d18O_out + SD_18O_out, xmax = d18O_out - SD_18O_out)) +
+    theme_figure()+
+    theme(
+      legend.position = "none"
+    )
+  
+  ggplotly(plt)
+}
 
 
 OutletValve_plt <- function(df){
