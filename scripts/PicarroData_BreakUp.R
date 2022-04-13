f5eabc9fb21fc85cc136ce5ce3d901694cf7fc89
Rachel Havranek <rachelhavranek@cu-besc-128.138.152.248.colorado.edu>
2022-04-13 10:15:31 -0600

    working through streamlining functions & writing the water concentration cut function

---
+++ scripts/PicarroData_BreakUp.R
@@ -0,0 +1,98 @@
+# A function to append time in MDT. Uses the lubridate package
+BoulderTime <- function(df){
+  df %>% 
+    mutate(UTC = hms(TIME), #by using the lubridate hms, it makes the UTC time filterable with dplyr
+           UTC_full = with_tz(ymd_hms(paste(DATE, TIME)), tzone = "UTC"), 
+           MDT = with_tz(UTC_full, tzone = "America/Denver"),
+           seconds = row_number()) 
+  
+}
+
+#given a time period, this function averages the relevant parameters
+flask_out_ave <- function (df) {
+  flask.out.ave <- df %>% summarise(
+    start_time = MDT[1],
+    SDH2O_out = round(sd(H2O), 2),
+    H2O_out = round(mean(H2O),2),
+    SD_18O_out = round(sd (Delta_18_16),2),
+    d18O_out = round(mean (Delta_18_16),2),
+    SD_2H_out = round(sd(Delta_D_H),2),
+    d2H_out = round(mean(Delta_D_H),2)
+  )
+  return(flask.out.ave)
+}
+
+
+#This function breaks up data when there is a clear break between flasks on a plot of OutletValve
+outlet_batches <- function (df) {
+  outlet.batches <- df %>% 
+    mutate(
+      seconds = row_number(),
+      tentimes = OutletValve-100 > as.numeric(c("", head(OutletValve, -1))),
+      tentimes = ifelse (is.na(tentimes), F, tentimes),
+      batch = cumsum(tentimes),
+      newbatch = as.character(batch)
+    )
+  return(outlet.batches)
+}
+
+
+# This function removes the first 100 seconds of a flask measurement, and then averages data across the next 3 minutes 
+flask_ave_outlet <- function (df) {
+  flask_aves <- df %>% 
+    group_by(batch) %>% 
+      mutate(
+        row = row_number(),
+        totalrows= n()) %>% 
+          filter(totalrows>100 & row > 90 & row < 271) %>% 
+            flask_out_ave()
+  
+  return(flask_aves)
+}
+
+#Very similar to outlet batches, but groups data by water concentration
+h2o_batches <- function (df) {
+  H2O.batches <- df %>% 
+  mutate(
+    seconds = row_number(),
+    waterdiff = H2O-100 > as.numeric(c("", head(H2O, -1))),
+    waterdiff = ifelse (is.na(waterdiff), F, waterdiff),
+    H2O_Batch = cumsum(waterdiff),
+    H2O_Newbatch = as.character(H2O_Batch)
+  )
+  
+  return(H2O.batches)
+}
+
+#Rather than taking the first clean 3 minutes of a measurement, this function looks at the last three minutes
+#this function is really helpful for the flask filling process 
+flask_last3mintues <- function(df) {
+  flask.aves <- df %>% 
+    group_by(H2O_Batch) %>% 
+      mutate( 
+        row = row_number(),
+        totalrows= n()
+      ) %>% 
+        filter (totalrows > 100) %>% 
+          filter(row < (totalrows - 60)) %>% 
+            filter(row > (totalrows - 240)) %>% 
+              flask_out_ave()
+    
+  return(flask.aves)  
+}
+
+#for a flask where its broken up by water concentration, but you want the first three minutes 
+flask_first3minutes <- function (df) {
+  flask.aves <- df %>% 
+    group_by(H2O_Batch) %>% 
+      mutate( 
+        row = row_number(),
+        totalrows= n()
+      ) %>% 
+        filter(totalrows>100 & row > 90 & row < 271) %>% 
+          flask_out_ave()
+}
+
+
+
+
--- scripts/Rachels_custom_plots.R
+++ scripts/Rachels_custom_plots.R
@@ -1,3 +1,16 @@
+#Works with the Outlet_batch function
+outlet_batch_plt <- function(df) {
+  plt <- df %>% 
+    ggplot()+
+    aes(x =MDT, y = OutletValve, color = newbatch) +
+    geom_point() +
+    theme_figure()
+  
+  ggplotly(plt, dynamicTicks = TRUE)
+}
+
+
+#plots the water concentration in a flask - most useful for a dry air test
 Flask_h2o_plt <- function (df){
   plt <- df %>% 
     ggplot()+
@@ -12,13 +25,13 @@ Flask_h2o_plt <- function (df){
   print(plt)
 }
 
-
+# works with teh flask_aves function, plots both d18O and d2H for each flask
 Flask_isotopes_plt <- function (df){
   plt <-df %>% 
     pivot_longer(cols = c(d18O_out, d2H_out)) %>% 
       ggplot()+
         aes(x = Flask, y = value, size = 2) +
-        facet_wrap (~name,scales = "free") +
+        facet_wrap (~name, scales = "free", ncol = 1) +
         geom_point()+
         scale_x_continuous(breaks = c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)) +
         theme_figure()+
@@ -56,10 +69,11 @@ dual_isotope_plt <- function (df){
       legend.position = "none"
     )
   
+  print(plt)
   ggplotly(plt)
 }
 
-
+#plots outlet valve vs. time. useful for seeing if using outlet batches vs. h2o batches will work better
 OutletValve_plt <- function(df){
   plt <- df %>% 
     ggplot() + 
@@ -70,6 +84,7 @@ OutletValve_plt <- function(df){
   ggplotly(plt, dynamicTicks = TRUE)
 }
   
+#plots water concentration through time 
 WaterConcentration_plt <- function(df){
   plt <- df %>% 
     ggplot() + 
@@ -79,4 +94,20 @@ WaterConcentration_plt <- function(df){
   
   ggplotly(plt, dynamicTicks = TRUE)
 }
+  
+h2o_batch_plt <- fucntion (df) {
+  plt <- df %>% 
+    ggplot()+
+    aes(x =MDT, y = H2O, color = H2O_Newbatch) +
+    geom_point() +
+    theme_figure()+
+    theme(
+      legend.position = "none"
+    )
+  
+  ggplotly(plt, dynamicTicks = TRUE)
+  print(plt)
+}
+  
+  
   
